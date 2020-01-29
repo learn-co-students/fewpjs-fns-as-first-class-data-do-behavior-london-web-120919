283882d625ab55f6c826750913a96ac0f6aafcc0
rmiri <re_miriuk@hotmail.com>
2020-01-29 11:45:50 +0000

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,22 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(hours) {
+  const splitHourMin = hours.split(":");
+  const hour = splitHourMin[0]
+  const integer = parseInt(hour, 10);
+
+    if (integer < 12) {
+      return "Good Morning"
+    } else if (integer >= 12 && integer <= 17 ) {
+      return "Good Afternoon"
+    } else {
+      return "Good Evening"
+    }
+}
+
+
 /* Write your implementation of displayMessage() */
+function displayMessage(string) {
+    document.getElementById("greeting").innerText = string;
+}
